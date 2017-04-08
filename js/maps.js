f22f99bfb0f609ba76f1f1ee9d94c721d56ebb52
parrycj <parrycj@vcu.edu>
2017-04-08 04:06:08 -0400

    Address to lat, long working. Still working on centering map at multiple points.

--- js/maps.js
+++ js/maps.js
@@ -1,14 +1,65 @@
 var map;
+
+//initializes map centered and zoomed at a specific point
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 37.787252, lng: -41.011963},
-        zoom: 3 // typical is usually 8 or 9
+        zoom: 8 // typical is usually 8 or 9
     });
 
+/*Centers maps at sets of points instead of one, we'll discuss
+    var locations = [
+        ['DESCRIPTION', 41.926979,12.517385, 3],
+        ['DESCRIPTION', 41.914873,12.506486, 2],
+        ['DESCRIPTION', 41.918574,12.507201, 1]
+    ];
 
-    var marker = new google.maps.Marker({
-        position: {lat: 38.433511, lng: -78.280334},
-        map: map,
-        icon: 'style/drawables/blueMapMarker.png'
+//create empty LatLngBounds object
+    var bounds = new google.maps.LatLngBounds();
+    var infowindow = new google.maps.InfoWindow();
+
+    for (i = 0; i < locations.length; i++) {
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map
+        });
+
+        //extend the bounds to include each marker's position
+        bounds.extend(marker.position);
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(locations[i][0]);
+                infowindow.open(map, marker);
+            }
+        })(marker, i));
+    }
+
+//now fit the map to the newly inclusive bounds
+    map.fitBounds(bounds);*/
+
+
+    var geocoder = new google.maps.Geocoder();
+    geocodeAddress(geocoder, map);
+}
+
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = "2300 W Grace St., Richmond, VA 23220"; //Make pull request from database and set address here
+    geocoder.geocode({'address': address}, function (results, status) {
+        if (status === 'OK') {
+            resultsMap.setCenter(results[0].geometry.location);
+            resultsMap.setZoom(18);
+            var marker = new google.maps.Marker({
+                map: resultsMap,
+                position: results[0].geometry.location,
+                icon: "style/drawables/blueMapMarker.png"
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
     });
 }
+
+
+
